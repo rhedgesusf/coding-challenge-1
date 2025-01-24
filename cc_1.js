6ff8bb967f34c568e1e379697914bb64fb0bc0f0
Riley Hedges <rhedges1@usf.edu>
2025-01-24 17:54:39 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -10,4 +10,14 @@ console.log("employeeName", employeeName, "type", typeof employeeName);
 console.log("employeeID", employeeID, "type", typeof employeeID);
 console.log("isActive", isActive, "type", typeof isActive);
 
+//////////////////////////////////
+// Task 2: Primitive Data Types //
+//////////////////////////////////
 
+let productName = "Cup";
+const productPrice = 4.64;
+var isAvailable = true;
+
+console.log("productName", productName, "type", typeof productName);
+console.log("productPrice", productPrice, "type", typeof productPrice);
+console.log("isAvailable", isAvailable, "type", typeof isAvailable);
