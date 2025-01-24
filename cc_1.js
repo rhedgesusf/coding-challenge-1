e9a44f7864a92c0582f799fc7068aae91d847b3b
Riley Hedges <rhedges1@usf.edu>
2025-01-24 18:01:58 -0500

    Task 3 - Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -21,3 +21,25 @@ var isAvailable = true;
 console.log("productName", productName, "type", typeof productName);
 console.log("productPrice", productPrice, "type", typeof productPrice);
 console.log("isAvailable", isAvailable, "type", typeof isAvailable);
+
+//////////////////////////////
+// Task 3: String Data Type //
+//////////////////////////////
+
+let accountBalance = 1500;
+console.log("Initial Value", accountBalance);
+
+accountBalance = accountBalance + 50;
+console.log("adding 50:", accountBalance);
+
+accountBalance = accountBalance - 100;
+console.log("subtracting 100:", accountBalance);
+
+accountBalance = accountBalance * 1.5;
+console.log("multiplying by 1.5:", accountBalance);
+
+accountBalance = accountBalance / 10;
+console.log("divided by 10:", accountBalance);
+
+
+
